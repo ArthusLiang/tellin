3b2995d68a62f263559e22f3230f5d2566a1131b
Arthus <531151995@qq.com>
2016-06-28 13:39:53 +0800

    add plan controller

---
+++ init/_controller/myController.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var fs = require("fs");
+var core = require('./tellin_core.js');
+var ejs = require('ejs');
+
+var myController = function(config,rule,data,view){
+
+};
+
+myController.prototype={
+	render:function(callback){
+
+	}
+};
+
+exports = module.exports = myController;
